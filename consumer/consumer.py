ed8bf6f542f1b36d0d1b10e3fbcec729d0d3bcf6
TevaJalink <108592111+TevaJalink@users.noreply.github.com>
2022-07-09 15:07:30 +0300

    Good Luck

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,4 +1,5 @@
 # this script is used to poll messages from sqs and push them as files to s3 bucket
+import time
 import boto3
 import json
 import botocore
@@ -18,8 +19,8 @@ queue_url = os.environ['QueueUrl']
 sqs_client = boto3.client("sqs", region_name=AWS_REGION,aws_access_key_id=aws_access_key_id,
                           aws_secret_access_key=aws_secret_access_key)
 # while is being used to create an infinite loop
-# response is the variable we use to grab the messages in the sqs
 while True:
+    # we use try and except for exception handling
     try:
         #response is the variable we use to grab the messages in the sqs
         response = sqs_client.receive_message(
@@ -27,8 +28,10 @@ while True:
         )
     except botocore.exceptions.ClientError:
         print('The IAM user does not have the required permissions to poll messages')
+        break
     except botocore.errorfactory.QueueDoesNotExist:
         print('The Queue Url is not correct or the queue does not exist')
+        break
     if 'Messages' in response:
         # filters the message Body from sqs message
         Body = response['Messages'][0]['Body']
@@ -45,7 +48,7 @@ while True:
         # Consumer_S3_Bucket_Name will define tha name of the s3 bucket
         Consumer_S3_Bucket_Name = os.environ['S3_Bucket_Name']
         Consumer_S3_Bucket_Name_String = str(Consumer_S3_Bucket_Name)
-        # line 17-19 is responsible for updating the csv to s3 bucket using boto3 library
+        # line 53-57 are responsible for updating the csv to s3 bucket using boto3 library
         try:
             s3 = boto3.resource('s3', region_name=AWS_REGION, aws_access_key_id=aws_access_key_id,
                                 aws_secret_access_key=aws_secret_access_key)
@@ -55,10 +58,12 @@ while True:
         except botocore.exceptions.ClientError:
             print('The IAM user does not have permissions to create objects in s3,'
                   ' The bucket name is incorrect or bucket does not exist')
+            break
         # now we will erase the message from sqs, http_status is the status code we get back from s3, if we dont get 200
-        # we would want to let the message back to the queue to be proccessed again
+        # we would want to let the message back to the queue to be processed again
         http_status = result['ResponseMetadata']['HTTPStatusCode']
         if http_status == 200:
+            # we use try and except for exception handling
             try:
                 # message_handle is the unique id of the message
                 message_handle = response['Messages'][0]['ReceiptHandle']
@@ -68,5 +73,9 @@ while True:
                 )
             except botocore.exceptions.ClientError:
                 print('The IAM user does not have permission to remove messages from the queue')
+                break
             except botocore.errorfactory.QueueDoesNotExist:
                 print('The Queue Url is not correct or the queue does not exist')
+                break
+    else:
+        time.sleep(5)
--- producer/Producer.py
+++ producer/Producer.py
@@ -25,9 +25,10 @@ def post():
     request_data = request.get_json()
     # body is turning the json to a string
     body = json.dumps(request_data)
-    # response is used to define the boto3 command to send a message to the sqs endpoint (QueueUrl is the endpoint url)
-    # and MessageBody is the content of the message
+    # adding try and except for exception handling
     try:
+        # response is used to define the boto3 command to send a message to the sqs endpoint (QueueUrl is the endpoint url)
+        # and MessageBody is the content of the message
         response = sqs_client.send_message(
             QueueUrl=queue_url,
             MessageBody=body
@@ -39,9 +40,6 @@ def post():
         print('The IAM user does not have permission to send messages to the queue')
     except botocore.errorfactory.QueueDoesNotExist:
         print('The Queue Url is not correct or the queue does not exist')
-
-
-# the if is running only when the API script is being used from the original path so debug mode would not run
-# when containarized
-if __name__ == "__main__":
-    app.run(debug=True)
+# app.run is responsible for the api to run when the code executes, it contains the host listening ip and ports
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
