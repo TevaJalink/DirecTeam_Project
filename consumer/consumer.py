cb0109fb5408d4385e2490477eccca7b5a89cdbe
TevaJalink <108592111+TevaJalink@users.noreply.github.com>
2022-07-09 18:24:49 +0300

    Good Luck

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,8 +1,6 @@
 # this script is used to poll messages from sqs and push them as files to s3 bucket
-import time
 import boto3
 import json
-import botocore
 import flatdict
 import os
 from datetime import datetime
@@ -16,33 +14,28 @@ AWS_REGION = 'us-east-1'
 # queue_url is used to define the endpoint of the sqs service
 queue_url = os.environ['QueueUrl']
 # sqs_client is used with the boto3 library in order to talk with sqs
-sqs_client = boto3.client("sqs", region_name=AWS_REGION,aws_access_key_id=aws_access_key_id,
+sqs_client = boto3.client("sqs", region_name=AWS_REGION, aws_access_key_id=aws_access_key_id,
                           aws_secret_access_key=aws_secret_access_key)
 # while is being used to create an infinite loop
+# inside the loop we try to take messages from the sqs queue and check for the key word Messages, if the word
+# exists then the function will take it, extract the json and push it as a csv to s3 bucket
+
 while True:
-    # we use try and except for exception handling
     try:
-        #response is the variable we use to grab the messages in the sqs
         response = sqs_client.receive_message(
             QueueUrl=queue_url
         )
-    except botocore.exceptions.ClientError:
+    except sqs_client.exceptions.ClientError:
         print('The IAM user does not have the required permissions to poll messages')
         break
-    except botocore.errorfactory.QueueDoesNotExist:
+    except sqs_client.errorfactory.QueueDoesNotExist:
         print('The Queue Url is not correct or the queue does not exist')
         break
     if 'Messages' in response:
-        # filters the message Body from sqs message
         Body = response['Messages'][0]['Body']
-        # json_format is going to change the Body from str to a dict
         json_format = json.loads(Body)
-        # flatend_dict changes the dict, so it would not have any nested objects using flatdict library
-        flatend_dict = flatdict.FlatDict(json_format, delimiter='.')
-        # file_content is the uploaded content the we would use, we convert it from a flatdict.FlatDict obj to a string
-        #  for the csv to look correct
-        File_content = str(flatend_dict)
-        # we use the date_time in order to give every file in s3 a different name
+        flattened_dict = flatdict.FlatDict(json_format, delimiter='.')
+        File_content = str(flattened_dict)
         date_time = datetime.now()
         date_time_str = str(date_time)
         # Consumer_S3_Bucket_Name will define tha name of the s3 bucket
@@ -55,27 +48,24 @@ while True:
             Create_File = s3.Object(Consumer_S3_Bucket_Name_String, 'post_processing/consumer_output ' + date_time_str
                                     + '.csv')
             result = Create_File.put(Body=str(File_content))
-        except botocore.exceptions.ClientError:
+        except s3.exceptions.ClientError:
             print('The IAM user does not have permissions to create objects in s3,'
                   ' The bucket name is incorrect or bucket does not exist')
             break
-        # now we will erase the message from sqs, http_status is the status code we get back from s3, if we dont get 200
-        # we would want to let the message back to the queue to be processed again
+        # now we will erase the message from sqs, http_status is the status code we get back from s3, if we don't get
+        # 200 we would want to let the message back to the queue to be processed again, we delete the messages based
+        # on the unique ID ReceiptHandle
         http_status = result['ResponseMetadata']['HTTPStatusCode']
         if http_status == 200:
-            # we use try and except for exception handling
             try:
-                # message_handle is the unique id of the message
                 message_handle = response['Messages'][0]['ReceiptHandle']
                 remove_messages = sqs_client.delete_message(
                     QueueUrl=queue_url,
                     ReceiptHandle=message_handle
                 )
-            except botocore.exceptions.ClientError:
+            except sqs_client.exceptions.ClientError:
                 print('The IAM user does not have permission to remove messages from the queue')
                 break
-            except botocore.errorfactory.QueueDoesNotExist:
+            except sqs_client.errorfactory.QueueDoesNotExist:
                 print('The Queue Url is not correct or the queue does not exist')
                 break
-    else:
-        time.sleep(5)
--- producer/Producer.py
+++ producer/Producer.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 import boto3
-import botocore
 import json
 import os
 
@@ -9,37 +8,37 @@ import os
 aws_access_key_id = os.environ['Producer_user_access_key_id']
 aws_secret_access_key = os.environ['Producer_user_secret_access_key']
 app = Flask(__name__)
+
 # AWS_REGION is used in order to define in which aws region we are working
 AWS_REGION = 'us-east-1'
 # queue_url defines the endpoint of the sqs service for us to send our messages to
 queue_url = os.environ['QueueUrl']
 # sqs_client is using the boto3 library to enable us to run commands on the sqs service
-sqs_client = boto3.client("sqs", region_name=AWS_REGION,aws_access_key_id=aws_access_key_id,
+sqs_client = boto3.client("sqs", region_name=AWS_REGION, aws_access_key_id=aws_access_key_id,
                           aws_secret_access_key=aws_secret_access_key)
 
 
 # @app.route is using the flask library to define which uri should we use and for what methods
+# inside the api we have a function the takes the json file from the post request and sends it to sqs
+# we use try and except for exception handling
+
 @app.route('/api', methods=['POST'])
 def post():
-    # request_data is using the request library in flask to receive the json file from the post command
     request_data = request.get_json()
-    # body is turning the json to a string
     body = json.dumps(request_data)
-    # adding try and except for exception handling
     try:
-        # response is used to define the boto3 command to send a message to the sqs endpoint (QueueUrl is the endpoint url)
-        # and MessageBody is the content of the message
         response = sqs_client.send_message(
             QueueUrl=queue_url,
             MessageBody=body
 
         )
-        # return is used to send back information to the client
         return (response)
-    except botocore.exceptions.ClientError:
+    except sqs_client.exceptions.ClientError:
         print('The IAM user does not have permission to send messages to the queue')
-    except botocore.errorfactory.QueueDoesNotExist:
+    except sqs_client.errorfactory.QueueDoesNotExist:
         print('The Queue Url is not correct or the queue does not exist')
+
 # app.run is responsible for the api to run when the code executes, it contains the host listening ip and ports
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
